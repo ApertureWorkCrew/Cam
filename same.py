7808c50ae01d00f192172f7b02a22b4b55aa102b
ApertureWorkCrew <yigitemremert@gmail.com>
2025-08-25 18:41:39 +0300

    Add files via upload

---
+++ same.py
@@ -0,0 +1,22 @@
+!DOCTYPE html
+html
+head
+  titleOBS Virtual Camera Testtitle
+head
+body
+  h1OBS Virtual Camera Feedh1
+  video id=video autoplay playsinlinevideo
+
+  script
+    async function startCamera() {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video true });
+        document.getElementById('video').srcObject = stream;
+      } catch (err) {
+        console.error(Error accessing camera, err);
+      }
+    }
+    startCamera();
+  script
+body
+html
